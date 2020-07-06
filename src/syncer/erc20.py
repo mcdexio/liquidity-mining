f159bc7ee05d55e8df53d148f892bdbb79158c13
heyanjun <realmeh@126.com>
2020-07-06 17:03:32 +0800

    add erc20 decimals adapt

--- src/syncer/erc20.py
+++ src/syncer/erc20.py
@@ -22,10 +22,11 @@ class ERC20Tracer(SyncerInterface):
         # contract
         self._erc20_token = ERC20Token(
             web3=web3, address=Address(token_address))
-
+        self._erc20_decimals = self._erc20_token.decimals
         self._logger = logging.getLogger()
         config.LOG_CONFIG["handlers"]["file_handler"]["filename"] = config.SYNCER_LOGPATH
         logging.config.dictConfig(config.LOG_CONFIG)
+    
 
     def _add_token_event(self, watcher_id, block_number, transaction_hash, token_address, event_index, transfer_type, holder, amount, db_session):
         token_event = TokenEvent()
@@ -39,24 +40,6 @@ class ERC20Tracer(SyncerInterface):
             amount = -amount
         token_event.amount = amount
         db_session.add(token_event)
-
-        #db_session.execute("refresh materialized view token_balances")
-        # update token_balances table, simulated materialized view
-        token_balance_item = db_session.query(TokenBalance)\
-            .filter(TokenBalance.holder == holder)\
-            .filter(TokenBalance.token == token_address)\
-                .first()
-        if token_balance_item is None:
-            token_balance_item = TokenBalance()
-            token_balance_item.watcher_id = watcher_id
-            token_balance_item.token = token_address
-            token_balance_item.holder = holder
-            token_balance_item.balance = amount
-        else:
-            token_balance_item.balance += amount
-        db_session.add(token_balance_item)
-        
-
 
         # update token_balances table, simulated materialized view
         token_balance_item = db_session.query(TokenBalance)\
@@ -84,7 +67,7 @@ class ERC20Tracer(SyncerInterface):
             transfer_info = row.args
             from_addr = transfer_info.get('from')
             to_addr = transfer_info.get('to')
-            amount = Decimal(transfer_info.get('value'))/Decimal(10**18)
+            amount = Decimal(transfer_info.get('value'))/Decimal(10**self._erc20_decimals)
             cur_block_number = row.blockNumber
             cur_transaction_hash = row.transactionHash
             event_index = row.logIndex
