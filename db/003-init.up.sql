8e4b41606d5a20ee79c45bcca63ac30c789c91d6
heyanjun <realmeh@126.com>
2020-08-05 18:33:20 +0800

    update sql 003

--- db/003-init.up.sql
+++ db/003-init.up.sql
@@ -5,12 +5,20 @@ INSERT INTO mining_rounds (round, begin_block_number, end_block_number, supply, 
 
 ALTER TABLE immature_mining_rewards ADD COLUMN pool_name text;
 UPDATE immature_mining_rewards  SET pool_name = 'ETH_PERP';
+ALTER TABLE immature_mining_rewards DROP CONSTRAINT immature_mining_rewards_pkey;
+ALTER TABLE immature_mining_rewards ADD CONSTRAINT immature_mining_rewards_pkey PRIMARY KEY (block_number, mining_round, holder, pool_name);
 
 ALTER TABLE immature_mining_reward_summaries ADD COLUMN pool_name text;
 UPDATE immature_mining_reward_summaries  SET pool_name = 'ETH_PERP';
+ALTER TABLE immature_mining_reward_summaries DROP CONSTRAINT immature_mining_reward_summaries_pkey;
+ALTER TABLE immature_mining_reward_summaries ADD CONSTRAINT immature_mining_reward_summaries_pkey PRIMARY KEY (mining_round, holder, pool_name);
 
 ALTER TABLE mature_mining_rewards ADD COLUMN pool_name text;
 UPDATE mature_mining_rewards  SET pool_name = 'ETH_PERP';
+ALTER TABLE immature_mining_rewards DROP CONSTRAINT immature_mining_rewards_pkey;
+ALTER TABLE immature_mining_rewards ADD CONSTRAINT immature_mining_rewards_pkey PRIMARY KEY (block_number, mining_round, holder, pool_name);
 
 ALTER TABLE mature_mining_reward_checkpoints ADD COLUMN pool_name text;
 UPDATE mature_mining_reward_checkpoints  SET pool_name = 'ETH_PERP';
+ALTER TABLE immature_mining_rewards DROP CONSTRAINT immature_mining_rewards_pkey;
+ALTER TABLE immature_mining_rewards ADD CONSTRAINT immature_mining_rewards_pkey PRIMARY KEY (block_number, mining_round, holder, pool_name);
