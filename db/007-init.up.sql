92fd33ab12162e649c7cc630f42684e5a439be71
heyanjun <realmeh@126.com>
2020-10-10 10:50:40 +0800

    HAN PERIOD extend 2 weeks

--- db/007-init.up.sql
+++ db/007-init.up.sql
@@ -4,3 +4,8 @@ INSERT INTO watchers (id, initial_block_number, synced_block_number) VALUES (104
 INSERT INTO mining_rounds (round, begin_block_number, end_block_number, supply, release_per_block, watcher_id, start_time, end_time, pool_supply) 
     VALUES ('HAN', 10932500, 11034999, 20500, 0.2, 6, '2020-09-26', '2020-10-11', '[{"name":"UNISWAP_MCB_USDC", "supply": 20500, "type": 2},{"name":"UNISWAP_MCB_ETH", "supply": 20500, "type": 2}]');
 
+
+update mining_rounds SET end_block_number=11137499  WHERE round='HAN';
+update mining_rounds SET supply = 41000  WHERE round='HAN';
+update mining_rounds SET end_time = '2020-10-26'  WHERE round='HAN';
+update mining_rounds set pool_supply = '[{"name":"UNISWAP_MCB_USDC", "supply": 41000, "type": 2},{"name":"UNISWAP_MCB_ETH", "supply": 41000, "type": 2}]' where round='HAN';
